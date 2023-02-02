e0b86e50aaa892e498c09ccbcb3ed11e57638f63
OtBot <info@o-blog.info>
2023-02-02 01:37:06 +0100

    minor fixxes

--- install_helper.sh
+++ install_helper.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 apt update
-apt install -y jq git
+apt install -y jq git samba samba-common-bin
+
+mkdir -p /home/pi/raspi-scan-proxy-to-seafile/scan
 
 cd /home/pi/ || exit
 
 git clone RedCore161/raspi-scan-proxy-to-seafile
 cd /home/pi/raspi-scan-proxy-to-seafile || exit
+
+#smbpasswd -a pi #TODO
 
 # Create Service
 cat <<EOT >> /etc/systemd/system/update_repo.service
@@ -22,7 +26,21 @@ TimeoutStartSec=0
 [Install]
 WantedBy=default.target
 EOT
+
+# Create Samba-Dir
+cat <<EOT >> /etc/samba/smb.conf
+[piscan]
+  comment = Pi Home
+  path = /home/pi/raspi-scan-proxy-to-seafile/scan
+  browseable = yes
+  writeable = yes
+  force create mode = 0777
+  force directory mode = 0777
+  public = yes
+EOT
+
 systemctl daemon-reload
 systemctl start update_repo.service
+systemctl restart smbd
 
 date '+%Y-%m-%d %H:%M:%S - Started Service' >> service.log
--- updater.sh
+++ updater.sh
@@ -5,5 +5,11 @@ cd /home/pi/raspi-scan-proxy-to-seafile || exit
 git pull
 date '+%Y-%m-%d %H:%M:%S - Pulled Repo' >> service.log
 
-bash /home/pi/raspi-scan-proxy-to-seafile/scan_folder.sh
-date '+%Y-%m-%d %H:%M:%S - Scanned Folder' >> service.log
+for i in {1..30}; do
+  bash /home/pi/raspi-scan-proxy-to-seafile/scan_folder.sh
+  RESULT=$?
+  echo "[RESULT] $RESULT $i/30"
+  date '+%Y-%m-%d %H:%M:%S - Scanned Folder' >> service.log
+  sleep 60
+done
+
